9e7f75ce4e4d5929e97e46165d38eda1a41f7c25
ruvcaltd <53894177+ruvcaltd@users.noreply.github.com>
2025-04-25 17:39:25 +0100

    Update directicenumberformat.ts

--- directicenumberformat.ts
+++ directicenumberformat.ts
@@ -1,18 +1,60 @@
-import { Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, forwardRef, HostListener, Input } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Directive({
   selector: '[appNumberFormat]',
-  standalone: true
-})
-export class NumberFormatDirective implements OnChanges {
-  @Input('appNumberFormat') numberValue!: number;
-
-  constructor(private el: ElementRef) {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['numberValue'] && this.numberValue != null) {
-      const formatted = new Intl.NumberFormat().format(this.numberValue);
-      this.el.nativeElement.innerText = formatted;
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NumberFormatDirective),
+      multi: true
     }
+  ]
+})
+export class NumberFormatDirective implements ControlValueAccessor {
+  private onChange = (value: any) => {};
+  private onTouched = () => {};
+
+  @Input() locale: string = 'en-US';
+
+  constructor(private el: ElementRef<HTMLInputElement>) {}
+
+  @HostListener('input', ['$event.target.value'])
+  onInput(rawValue: string) {
+    const parsed = Number(rawValue.replace(/[^\d.-]/g, ''));
+    this.onChange(parsed);
+  }
+
+  @HostListener('blur')
+  onBlur() {
+    this.onTouched();
+    const value = this.el.nativeElement.value;
+    const parsed = Number(value.replace(/[^\d.-]/g, ''));
+    this.el.nativeElement.value = this.format(parsed);
+  }
+
+  writeValue(value: any): void {
+    if (value !== null && value !== undefined) {
+      this.el.nativeElement.value = this.format(value);
+    } else {
+      this.el.nativeElement.value = '';
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.el.nativeElement.disabled = isDisabled;
+  }
+
+  private format(value: number): string {
+    return new Intl.NumberFormat(this.locale).format(value);
   }
 }
